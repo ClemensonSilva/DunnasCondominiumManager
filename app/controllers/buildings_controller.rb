322adeb1dbf89ccbff2f2b6b59d3a114cf40e247
Clemerson <clemesonsilva736@gmail.com>
2026-04-12 14:55:23 -0300

    feat: atualizei parametros das views para usar os novos nomes apartments_per_floor ao invés de number_of_apartments, por exemplo

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -65,6 +65,6 @@ class BuildingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def building_params
-      params.expect(building: [ :name, :number_of_apartments, :number_of_floors ])
+      params.expect(building: [ :name, :apartments_per_floor, :number_of_floors ])
     end
 end
--- app/views/buildings/_building.json.jbuilder
+++ app/views/buildings/_building.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! building, :id, :name, :condominium_id, :number_of_apartments, :number_of_floors, :created_at, :updated_at
+json.extract! building, :id, :name, :apartments_per_floor, :number_of_floors, :created_at, :updated_at
 json.url building_url(building, format: :json)
