fabeee0807f3ba7eb77c0af12695fa95ccfec047
Clemerson <clemesonsilva736@gmail.com>
2026-04-16 23:21:10 -0300

    refactor: reorganizei as autorizações do cancan na ability para deixar mais organizado e legivel a regra de negocio

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,44 +4,55 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    super()
-
     user ||= User.new
-
     return unless user.persisted?
-
-    if user.admin?
-      can :manage, :all
-      return
-    end
 
     can %i[read update], User, id: user.id
 
-    if user.colaborator?
-      can %i[read ], Ticket, ticket_type: { scope_id: user.scope_ids }
-      can :take, Ticket,
-          ticket_type: { scope_id: user.scope_ids },
-          finished_at: nil,
-          collaborator_id: nil
-      can :finalize, Ticket, collaborator_id: user.id, finished_at: nil
-      can :read, Comment, ticket: { ticket_type: { scope_id: user.scope_ids } }
-      can :create, Comment, ticket: { ticket_type: { scope_id: user.scope_ids } }
-      can %i[update destroy], Comment,
-          user_id: user.id,
-          ticket: { ticket_type: { scope_id: user.scope_ids } }
-      return
+    if user.admin?
+      admin_abilities
+    elsif user.colaborator?
+      collaborator_abilities(user)
+    elsif user.resident?
+      resident_abilities(user)
     end
+  end
 
-    if user.resident?
-      can :read, [ Building ]
-      can :create, Ticket
-      can %i[read update destroy], Ticket, apartment_id: user.apartment_ids
-      can :read, Comment, ticket: { apartment_id: user.apartment_ids }
-      can :create, Comment, ticket: { apartment_id: user.apartment_ids }
-      can %i[update destroy], Comment,
-          user_id: user.id,
-          ticket: { apartment_id: user.apartment_ids }
-      nil
-    end
+  private
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def collaborator_abilities(user)
+    can :read, Ticket, ticket_type: { scope_id: user.scope_ids }
+
+    can :take, Ticket,
+        ticket_type: { scope_id: user.scope_ids },
+        finished_at: nil,
+        collaborator_id: nil
+
+    can :finalize, Ticket,
+        collaborator_id: user.id,
+        finished_at: nil
+
+    can %i[read create], Comment, ticket: { ticket_type: { scope_id: user.scope_ids } }
+
+    can %i[update destroy], Comment,
+        user_id: user.id,
+        ticket: { ticket_type: { scope_id: user.scope_ids } }
+  end
+
+  def resident_abilities(user)
+    can :read, Building
+
+    can :create, Ticket
+    can %i[read update destroy], Ticket, apartment_id: user.apartment_ids
+
+    can %i[read create], Comment, ticket: { apartment_id: user.apartment_ids }
+
+    can %i[update destroy], Comment,
+        user_id: user.id,
+        ticket: { apartment_id: user.apartment_ids }
   end
 end
